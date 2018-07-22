6730a1fa9b97aa963d0b204e6a34042c4794beff
alejomg <alejandromg@gmail.com>
2018-07-22 11:36:04 -0700

    Added FormsModule. Added an input element to mineral details area

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // <-- NgModel isn't available by default, it must opt-in to using it.
 
 import { AppComponent } from './app.component';
 import { MineralComponent } from './mineral/mineral.component';
@@ -10,7 +11,8 @@ import { MineralComponent } from './mineral/mineral.component';
     MineralComponent
   ],
   imports: [
-    BrowserModule
+	BrowserModule,
+	FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
